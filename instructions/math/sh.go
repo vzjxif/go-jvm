c9ef28686b175a8f4bb11e4f0dd4882ec95d3ccc
vzjxif <zjc.pers@hotmail.com>
2022-08-02 21:58:20 +0800

    feat: add sh

---
+++ instructions/math/sh.go
@@ -0,0 +1,12 @@
+package math
+
+import (
+	"github.com/vzjxif/go-jvm/instructions/base"
+)
+
+// int 左位移
+type ISHL struct{ base.NoOperandsInstruction }
+
+type ISHR struct{ base.NoOperandsInstruction }
+
+type IUSHR struct{ base.NoOperandsInstruction }
